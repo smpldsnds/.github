747348835d37c452f8236e4c3e065a24ad3430b9
danigb <danigb@gmail.com>
2023-09-17 14:22:42 +0200

    feat: add sfz_files.json

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -7,12 +7,28 @@ def create_json_files(directory, destructive):
     create_index_json_files(directory, instruments, destructive)
 
     instruments = [x.replace(directory + "/", "") for x in instruments]
-    output_path = os.path.join(directory, 'instruments.json')
+    instruments_path = os.path.join(directory, 'instruments.json')
     if destructive:
-        with open(output_path, 'w') as json_file:
+        with open(instruments_path, 'w') as json_file:
             json.dump(instruments, json_file)
+    print(f"index.json created: {instruments_path}")
 
-    print(f"index.json created: {output_path}")
+    sfz_files = find_all_sfz_files(directory)
+    sfz_files = [x.replace(directory + "/", "").replace(".sfz", "")
+                 for x in sfz_files]
+    sfz_files_path = os.path.join(directory, 'sfz_files.json')
+    if destructive:
+        with open(sfz_files_path, 'w') as json_file:
+            json.dump(sfz_files, json_file)
+
+
+def find_all_sfz_files(directory):
+    sfz_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.sfz'):
+                sfz_files.append(os.path.join(root, file))
+    return sfz_files
 
 
 def create_index_json_files(directory, instruments, destructive):
@@ -21,13 +37,13 @@ def create_index_json_files(directory, instruments, destructive):
         directory) if file.endswith('.ogg')]
 
     if ogg_files:
-        output_path = os.path.join(directory, 'files.json')
+        instruments_path = os.path.join(directory, 'files.json')
         instruments.append(directory)
         if destructive:
             # Save the list of filenames to an index.json file inside the directory
-            with open(output_path, 'w') as json_file:
+            with open(instruments_path, 'w') as json_file:
                 json.dump(ogg_files, json_file)
-        print(f"files.json created: {output_path}")
+        print(f"files.json created: {instruments_path}")
 
     # Check and process nested subdirectories
     for item in os.listdir(directory):
